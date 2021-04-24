06e53f668063b8501b6a4bcb6d7ba2553097d618
vineboneto <vineboneto@gmail.com>
2021-04-24 10:24:07 -0300

    chore: add husky

---
+++ husky-config.js
@@ -0,0 +1,19 @@
+const { execSync } = require('child_process')
+
+console.log('Setting Husky...')
+const scripts = [
+  'npx rimraf .config',
+  'npx husky install .config/husky',
+  'npx husky add .config/husky/pre-commit "yarn lint-staged --config .lintstagedrc.json"',
+  'npx husky add .config/husky/commit-msg ".git/hooks/commit-msg $1"'
+]
+
+for (script of scripts) {
+  try {
+    console.log(script + '👀')
+    execSync(script)
+    console.log('✔')
+  } catch (error) {
+    console.error
+  }
+}
