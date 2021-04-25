aa4fad478914eaa50ed8d133fa62a32b4cc1dcc4
vineboneto <vineboneto@gmail.com>
2021-04-25 12:05:51 -0300

    test: ensure HttpClient return the correct response on axios

--- tests/infra/axios-http-client/axios-http-client.spec.ts
+++ tests/infra/axios-http-client/axios-http-client.spec.ts
@@ -32,4 +32,14 @@ describe('AxiosHttpClient', () => {
       method: request.method
     })
   })
+
+  test('Should return the correct response on axios', async () => {
+    const { sut, mockedAxios } = makeSut()
+    const httpResponse = await sut.request(mockHttpRequest())
+    const axiosResponse = await mockedAxios.request.mock.results[0].value
+    expect(httpResponse).toEqual({
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    })
+  })
 })
