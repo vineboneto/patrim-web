d0291f1d5bd3b2c51c0514192840870a28d53ef1
vineboneto <vineboneto@gmail.com>
2021-04-25 12:00:41 -0300

    test: ensure AxiosHttpClient call axios with correct values

--- tests/data/mocks/mock-http.ts
+++ tests/data/mocks/mock-http.ts
@@ -1,5 +1,14 @@
 import { HttpClient, HttpRequest, HttpResponse, HttpStatusCode } from '@/data/protocols'
 
+import faker from 'faker'
+
+export const mockHttpRequest = (): HttpRequest => ({
+  url: faker.internet.url(),
+  method: faker.random.arrayElement(['post', 'get', 'put', 'delete']),
+  body: faker.random.objectElement(),
+  headers: faker.random.objectElement()
+})
+
 export class HttpClientSpy<R = any> implements HttpClient<R> {
   params: HttpRequest
   response: HttpResponse<R> = {
---
+++ tests/infra/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,35 @@
+import { AxiosHttpClient } from '@/infra/http/axios-http-client'
+import { mockHttpRequest } from '@/tests/data/mocks'
+import { mockAxios } from '@/tests/infra/mocks'
+
+import axios from 'axios'
+
+jest.mock('axios')
+
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
+  return {
+    sut,
+    mockedAxios
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct values', async () => {
+    const { sut, mockedAxios } = makeSut()
+    const request = mockHttpRequest()
+    await sut.request(request)
+    expect(mockedAxios.request).toHaveBeenCalledWith({
+      url: request.url,
+      data: request.body,
+      headers: request.headers,
+      method: request.method
+    })
+  })
+})
