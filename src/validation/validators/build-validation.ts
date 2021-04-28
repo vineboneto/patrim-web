332e3f5951b48fbdabd35ad888182aaf5fa130f6
vineboneto <vineboneto@gmail.com>
2021-04-28 12:01:39 -0300

    feat: create build validation

---
+++ src/validation/validators/build-validation.ts
@@ -0,0 +1,42 @@
+import {
+  RequiredFieldValidation,
+  EmailValidation,
+  MinLengthValidation,
+  CompareFieldValidation
+} from '@/validation/validators'
+import { FieldValidation } from '@/validation/protocols'
+
+export class ValidationBuilder {
+  private constructor (
+    private readonly fieldName: string,
+    private readonly validations: FieldValidation[]
+  ) {}
+
+  static field (fieldName: string): ValidationBuilder {
+    return new ValidationBuilder(fieldName, [])
+  }
+
+  required (): ValidationBuilder {
+    this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  email (): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName))
+    return this
+  }
+
+  min (length: number): ValidationBuilder {
+    this.validations.push(new MinLengthValidation(this.fieldName, length))
+    return this
+  }
+
+  sameAs (fieldToCompare: string): ValidationBuilder {
+    this.validations.push(new CompareFieldValidation(this.fieldName, fieldToCompare))
+    return this
+  }
+
+  build (): FieldValidation[] {
+    return this.validations
+  }
+}
