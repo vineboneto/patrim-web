b8d47d4c14b23aee2b77f0fb20bdc46b08afe9b8
vineboneto <vineboneto@gmail.com>
2021-04-28 11:33:20 -0300

    feat: ensure CompareFieldValidation return null is invalid fields

---
+++ src/validation/validators/compare-field-validation.ts
@@ -0,0 +1,13 @@
+import { FieldValidation } from '@/validation/protocols'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class CompareFieldValidation implements FieldValidation {
+  constructor (
+    readonly field: string,
+    private readonly fieldToCompare: string
+  ) {}
+
+  validate (input: object): Error {
+    return input[this.field] === input[this.fieldToCompare] ? null : new InvalidFieldError()
+  }
+}
--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,2 +1,3 @@
 export * from './required-field-validation'
 export * from './min-length-validation'
+export * from './compare-field-validation'
