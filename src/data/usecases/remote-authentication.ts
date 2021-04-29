4ec35a4063e237901cda3daca50e04de01a18ed2
vineboneto <vineboneto@gmail.com>
2021-04-29 20:36:14 -0300

    feat: ensure RemoteAuthentication return an account if HttpClient returns 200

--- src/data/usecases/remote-authentication.ts
+++ src/data/usecases/remote-authentication.ts
@@ -15,7 +15,7 @@ export class RemoteAuthentication implements Authentication {
       url: this.url
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
