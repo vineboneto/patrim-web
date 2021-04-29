a39ef856435d74645339f3c8ea45018248874f68
vineboneto <vineboneto@gmail.com>
2021-04-28 23:22:20 -0300

    test: ensure LoadStorageAdapter call localStorage.setItem with correct value

--- tests/infra/cache/load-storage-adapter.spec.ts
+++ tests/infra/cache/load-storage-adapter.spec.ts
@@ -6,13 +6,25 @@ import 'jest-localstorage-mock'
 const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
 
 describe('LoadStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
   test('Should call localStorage.getItem with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
-    const value = faker.random.objectElement()
+    const value = faker.random.objectElement<{}>()
     const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
     const obj = sut.get(key)
     expect(obj).toEqual(value)
     expect(getItemSpy).toHaveBeenCalledWith(key)
+  })
+
+  test('Should call localStorage.setItem with correct value', () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = faker.random.objectElement<{}>()
+    sut.set(key, value)
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
   })
 })
