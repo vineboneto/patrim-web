a6946b6d007e6dd20adecd1c54c9a4b7b4d25e22
vineboneto <vineboneto@gmail.com>
2021-04-29 00:08:38 -0300

    test: ensure CompareFieldValidation return null if valid fields

--- tests/validation/validators/compare-field-validation.spec.ts
+++ tests/validation/validators/compare-field-validation.spec.ts
@@ -5,12 +5,21 @@ const makeSut = (field: string, fieldToCompare: string):
 CompareFieldValidation => new CompareFieldValidation(field, fieldToCompare)
 
 describe('CompareFieldValidation', () => {
-  test('Should return null is invalid fields', () => {
+  test('Should return error is invalid fields', () => {
     const sut = makeSut('field', 'fieldToCompare')
     const error = sut.validate({
       field: 'any_value',
       fieldToCompare: 'other_value'
     })
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return null if valid fields', () => {
+    const sut = makeSut('field', 'fieldToCompare')
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(error).toBeNull()
   })
 })
