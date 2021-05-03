d8e4837964e658d1c38622b2c6ed43c478c49c37
vineboneto <vineboneto@gmail.com>
2021-05-03 00:15:10 -0300

    feat: ensure Home component go to login page on exit

--- src/presentation/components/header/components/user/user.tsx
+++ src/presentation/components/header/components/user/user.tsx
@@ -1,17 +1,24 @@
 import './user-styles.css'
 
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import Typography from '@material-ui/core/Typography'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import IconButton from '@material-ui/core/IconButton'
 
 const User: React.FC = () => {
+  const history = useHistory()
+
+  const handleExit = (): void => {
+    history.replace('/login')
+  }
+
   return (
     <div className="user-wrap">
       <Typography color="inherit">
         Vinicius
       </Typography>
-      <IconButton color="inherit">
+      <IconButton color="inherit" data-testid="exit-link" onClick={handleExit}>
         <ExitToAppIcon />
       </IconButton>
     </div>
