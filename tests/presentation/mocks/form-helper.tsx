e7d0c0a67b355aff205f724e05f84a081dda2a91
vineboneto <vineboneto@gmail.com>
2021-05-05 03:28:04 -0300

    test: ensure PatrimonyCreate start with initial state

--- tests/presentation/mocks/form-helper.tsx
+++ tests/presentation/mocks/form-helper.tsx
@@ -8,6 +8,11 @@ export const testStatusForField = (fieldName: string, validationError: string = 
   expect(field).toHaveProperty('title', validationError)
 }
 
+export const testStatusForFieldSelect = (fieldName: string, validationError: string = ''): void => {
+  const field = screen.getByTestId(fieldName)
+  expect(field).toHaveProperty('title', validationError)
+}
+
 export const populateField = (fieldName: string, value = faker.random.word()): void => {
   const field = screen.getByTestId(fieldName)
   const input = field.children[1].children[0]
---
+++ tests/presentation/pages/patrimony-create.spec.tsx
@@ -0,0 +1,50 @@
+import { PatrimonyCreate } from '@/presentation/pages'
+import { ApiContext } from '@/presentation/components'
+import { testStatusForField, testStatusForFieldSelect, ValidationStub } from '@/tests/presentation/mocks'
+import { mockAccountModel } from '@/tests/domain/mocks'
+
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import { render, screen } from '@testing-library/react'
+import faker from 'faker'
+
+type Params = {
+  validationError: string
+}
+
+type SutTypes = {
+  validationStub: ValidationStub
+}
+
+const makeSut = (params?: Params): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/patrimonies/new'] })
+  const setCurrentAccountMock = jest.fn()
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
+      <Router history={history}>
+        <PatrimonyCreate
+          validation={validationStub}
+        />
+      </Router>
+    </ApiContext.Provider>
+  )
+  return {
+    validationStub
+  }
+}
+
+describe('PatrimonyCreate', () => {
+  test('Should start with initial state', () => {
+    const validationError = faker.random.words()
+    makeSut({ validationError })
+    expect(screen.getByTestId('error-wrap').children).toHaveLength(0)
+    expect(screen.getByTestId('submit')).toBeDisabled()
+    testStatusForField('number', validationError)
+    testStatusForField('brand', validationError)
+    testStatusForFieldSelect('owner', validationError)
+    testStatusForFieldSelect('category', validationError)
+  })
+})
