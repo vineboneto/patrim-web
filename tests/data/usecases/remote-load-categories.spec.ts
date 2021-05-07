832487a499c3aa0fca50f01a99386d528fdd15cb
vineboneto <vineboneto@gmail.com>
2021-05-07 00:26:52 -0300

    test: ensure RemoteLoadCategories throw UnexpectedError if HttpClient returns 500

--- tests/data/usecases/remote-load-categories.spec.ts
+++ tests/data/usecases/remote-load-categories.spec.ts
@@ -46,6 +46,15 @@ describe('RemoteLoadCategories', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
+  test('Should throw UnexpectedError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
   test('Should throw UnexpectedError if HttpClient returns 404', async () => {
     const { sut, httpClientSpy } = makeSut()
     httpClientSpy.response = {
