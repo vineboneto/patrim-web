91c0637915185cf0ffb29a1d382e39849b85d7af
vineboneto <vineboneto@gmail.com>
2021-05-09 00:22:26 -0300

    test: ensure RemoteLoadOwners throw UnexpectedError if HttpClient returns 500

--- tests/data/usecases/remote-load-owners.spec.ts
+++ tests/data/usecases/remote-load-owners.spec.ts
@@ -45,4 +45,13 @@ describe('RemoteLoadOwners', () => {
     const promise = sut.load()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
