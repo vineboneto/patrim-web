6bc5fa9cc1c11c24f339208363e9e3c0efff112b
vineboneto <vineboneto@gmail.com>
2021-05-12 11:42:59 -0300

    test: ensure RemoteLoadPatrimonies call HttpClient Client with correct values

---
+++ tests/data/usecases/remote-load-patrimonies.spec.ts
@@ -0,0 +1,28 @@
+import { RemoteLoadPatrimonies } from '@/data/usecases'
+import { HttpClientSpy } from '@/tests/data/mocks'
+
+import faker from 'faker'
+
+type SutTypes = {
+  sut: RemoteLoadPatrimonies
+  httpClientSpy: HttpClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy()
+  const sut = new RemoteLoadPatrimonies(httpClientSpy, url)
+  return {
+    sut,
+    httpClientSpy
+  }
+}
+
+describe('RemoteLoadPatrimonies', () => {
+  test('Should call HttpClient Client with correct values', async () => {
+    const url = faker.internet.url()
+    const { sut, httpClientSpy } = makeSut(url)
+    await sut.load()
+    expect(httpClientSpy.params.method).toBe('get')
+    expect(httpClientSpy.params.url).toBe(url)
+  })
+})
