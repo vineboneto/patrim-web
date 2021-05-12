3c2cd6d1d132ba4dbbcb8707394f601fecc99d25
vineboneto <vineboneto@gmail.com>
2021-05-12 11:49:42 -0300

    test: ensure RemoteLoadPatrimonies throw UnexpectedError if HttpClient returns 400

--- tests/data/usecases/remote-load-patrimonies.spec.ts
+++ tests/data/usecases/remote-load-patrimonies.spec.ts
@@ -1,6 +1,6 @@
 import { RemoteLoadPatrimonies } from '@/data/usecases'
 import { HttpStatusCode } from '@/data/protocols'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { HttpClientSpy } from '@/tests/data/mocks'
 
 import faker from 'faker'
@@ -35,5 +35,14 @@ describe('RemoteLoadPatrimonies', () => {
     }
     const promise = sut.load()
     await expect(promise).rejects.toThrow(new AccessDeniedError())
+  })
+
+  test('Should throw UnexpectedError if HttpClient returns 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
