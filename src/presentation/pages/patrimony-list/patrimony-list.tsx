8e68926112af0f0f1fb37a0c1d437fe373bcf2dc
vineboneto <vineboneto@gmail.com>
2021-05-12 12:31:49 -0300

    feat: add styles to patrimony list

--- src/presentation/pages/patrimony-list/patrimony-list.tsx
+++ src/presentation/pages/patrimony-list/patrimony-list.tsx
@@ -1,4 +1,5 @@
 import { DashboardContext, Header, DashBoard } from '@/presentation/components'
+import './patrimony-list-styles.css'
 
 import React, { useState } from 'react'
 
@@ -8,10 +9,24 @@ const PatrimonyList: React.FC = () => {
   })
 
   return (
-    <DashboardContext.Provider value={{ state, setState }}>
-      <Header title="Buscar Patrimônio" />
-      <DashBoard />
-    </DashboardContext.Provider>
+    <div className="patrimony-list-wrap">
+      <DashboardContext.Provider value={{ state, setState }}>
+        <Header title="Buscar Patrimônio" />
+        <DashBoard />
+      </DashboardContext.Provider>
+      <div className="div patrimony-items-wrap">
+        <div className="patrimony-item-wrap">
+          <div className="patrimony-item-body">
+            <h5>86521</h5>
+            <p>Computador: Dell</p>
+            <p>Proprietário: Vinicius</p>
+            <p>Setor: UPA</p>
+            <a href="#">Editar</a>
+            <a href="#">Excluir</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
