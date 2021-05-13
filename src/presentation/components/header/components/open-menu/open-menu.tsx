81ed926e9403992cdb25192231b052ad2220647d
vineboneto <vineboneto@gmail.com>
2021-05-13 01:26:03 -0300

    refactor: update layout header

--- src/presentation/components/header/components/open-menu/open-menu.tsx
+++ src/presentation/components/header/components/open-menu/open-menu.tsx
@@ -3,11 +3,13 @@ import './open-menu-styles.css'
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const OpenMenu: React.FC = () => {
   return (
     <div className="menu-wrap" data-testid="menu">
       <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon />
         <Typography variant="h6" color="inherit">
           Patrim
         </Typography>
