011fd85779b58deee5297d72fb1dab6a5e57ac93
vineboneto <vineboneto@gmail.com>
2021-05-13 23:11:35 -0300

    refactor: add fields to search of patrimonies

--- src/presentation/pages/patrimony-list/patrimony-list.tsx
+++ src/presentation/pages/patrimony-list/patrimony-list.tsx
@@ -1,8 +1,8 @@
 import './patrimony-list-styles.css'
 
-import { Header } from '@/presentation/components'
+import { Header, Input, FormContext } from '@/presentation/components'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import Pagination from '@material-ui/lab/Pagination'
@@ -10,14 +10,39 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit'
 
 const PatrimonyList: React.FC = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    number: '',
+    category: '',
+    owner: ''
+  })
+
   return (
     <div className="patrimony-list-wrap">
       <Header title="Patrimônios" />
       <div className="container patrimony-list-content">
         <div className="row gy-4">
+          <FormContext.Provider value={{ state, setState }}>
+            <form className="form-content">
+              <div className="row">
+                <div className="col-12 col-lg-3">
+                  <Input type="text" name="owner" placeholder="Proprietário" />
+                </div>
+                <div className="col-12 col-lg-3">
+                  <Input type="text" name="number" placeholder="Número" />
+                </div>
+                <div className="col-12 col-lg-3">
+                  <Input name="category" placeholder="Categoria" />
+                </div>
+                <div className="col-12 col-lg-3">
+                  <Button variant="outlined" color="primary" style={{ width: '100%', height: '56px' }}>Pesquisar</Button>
+                </div>
+              </div>
+            </form>
+          </FormContext.Provider>
           <div className="col-12">
             <Link to="/patrimonies/new">
-              <Button variant="contained" color="primary" style={{ width: '100%' }}>Novo</Button>
+              <Button variant="contained" color="primary" style={{ width: '100%', height: '56px' }}>Novo</Button>
             </Link>
           </div>
           <div className="col-12 col-md-6 col-lg-4">
