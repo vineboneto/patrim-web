1e0194138afa6236b3e6761ebc603434ac140a65
vineboneto <vineboneto@gmail.com>
2021-05-14 20:55:35 -0300

    test: ensure PatrimonyList start with initial state

--- tests/presentation/pages/patrimony-list.spec.tsx
+++ tests/presentation/pages/patrimony-list.spec.tsx
@@ -19,9 +19,12 @@ const makeSut = (): void => {
   )
 }
 
-describe('PatrimonyCreate Component', () => {
+describe.only('PatrimonyList Component', () => {
   test('Should start with initial state', () => {
     makeSut()
     expect(screen.getByTestId('spinner')).toBeInTheDocument()
+    expect(screen.getByTestId('owner').children[0].children[1].children[0].getAttribute('value')).toBe('')
+    expect(screen.getByTestId('category').children[0].children[1].children[0].getAttribute('value')).toBe('')
+    expect(screen.getByTestId('number').children[1].children[0].getAttribute('value')).toBe('')
   })
 })
