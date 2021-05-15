f3d30e3e9a489053df741d05eea6add7c119097e
vineboneto <vineboneto@gmail.com>
2021-05-14 23:17:42 -0300

    refactor: remove params of factory load patrimonies

--- src/main/factories/pages/patrimony-list/patrimony-list-factory.tsx
+++ src/main/factories/pages/patrimony-list/patrimony-list-factory.tsx
@@ -2,19 +2,11 @@ import { makeRemoteLoadPatrimonies } from '@/main/factories/usecases'
 import { PatrimonyList } from '@/presentation/pages'
 
 import React from 'react'
-import { useParams } from 'react-router-dom'
-
-type Params = {
-  take: string
-  skip: string
-}
 
 export const makePatrimonyList: React.FC = () => {
-  const { skip, take } = useParams<Params>()
-
   return (
     <PatrimonyList
-      loadPatrimonies={makeRemoteLoadPatrimonies(skip, take)}
+      loadPatrimonies={makeRemoteLoadPatrimonies()}
     />
   )
 }
--- src/main/factories/usecases/remote-load-patrimonies-factory.ts
+++ src/main/factories/usecases/remote-load-patrimonies-factory.ts
@@ -2,9 +2,9 @@ import { RemoteLoadPatrimonies } from '@/data/usecases'
 import { makeAuthorizeHttpClientDecorator } from '@/main/factories/decorators'
 import { makeApiUrl } from '@/main/factories/http'
 
-export const makeRemoteLoadPatrimonies = (take: string , skip: string): RemoteLoadPatrimonies => {
+export const makeRemoteLoadPatrimonies = (): RemoteLoadPatrimonies => {
   return new RemoteLoadPatrimonies(
     makeAuthorizeHttpClientDecorator(),
-    makeApiUrl(`/patrimonies?skip=${skip}&take=${take}`)
+    makeApiUrl('/patrimonies')
   )
 }
