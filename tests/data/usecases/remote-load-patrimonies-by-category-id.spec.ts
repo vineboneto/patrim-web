3ab89c82aa32d9b297befc616bad4ec1b1c58f87
vineboneto <vineboneto@gmail.com>
2021-05-16 01:56:39 -0300

    test: ensure RemoteLoadPatrimoniesByCategoryId throw AccessDeniedError if HttpGetClient returns 403

--- tests/data/usecases/remote-load-patrimonies-by-category-id.spec.ts
+++ tests/data/usecases/remote-load-patrimonies-by-category-id.spec.ts
@@ -1,8 +1,10 @@
 import { RemoteLoadPatrimoniesByCategoryId } from '@/data/usecases'
+import { HttpStatusCode } from '@/data/protocols'
 import { HttpClientSpy } from '@/tests/data/mocks'
 import { mockLoadPatrimoniesByCategoryIdParams } from '@/tests/domain/mocks'
 
 import faker from 'faker'
+import { AccessDeniedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteLoadPatrimoniesByCategoryId
@@ -26,5 +28,14 @@ describe('RemoteLoadPatrimoniesByCategoryId', () => {
     await sut.loadByCategoryId(params)
     expect(httpClientSpy.params.method).toBe('get')
     expect(httpClientSpy.params.url).toBe(`${url}/${params.id}/patrimonies?take=${params.take}&skip=${params.skip}`)
+  })
+
+  test('Should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.loadByCategoryId(mockLoadPatrimoniesByCategoryIdParams())
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
