9775e8a9911bb01890cb94074fe7ae06941e3a97
vineboneto <vineboneto@gmail.com>
2021-05-16 14:28:51 -0300

    refactor: remove duplicated code

--- src/presentation/pages/patrimony-list/patrimony-list.tsx
+++ src/presentation/pages/patrimony-list/patrimony-list.tsx
@@ -19,6 +19,7 @@ import {
 } from '@/domain/usecases'
 
 import React, { useState, useEffect } from 'react'
+import { PatrimonyModel } from '@/domain/models'
 
 type Props = {
   loadPatrimonies: LoadPatrimonies
@@ -53,6 +54,53 @@ const PatrimonyList: React.FC<Props> = ({
     patrimonies: [] as ItemProps[]
   })
 
+  const setPatrimonies = (patrimonies: PatrimonyModel[]): void => {
+    setState(old => ({
+      ...old,
+      isLoading: false,
+      patrimonies: patrimonies.map((p) => ({
+        id: p.id.toString(),
+        brand: p.brand,
+        number: p.number,
+        category: p.category.name,
+        owner: p.owner.name,
+        sector: p.owner.sector.name
+      }))
+    }))
+  }
+
+  const setPatrimony = (patrimony: PatrimonyModel): void => {
+    setState(old => ({
+      ...old,
+      totalPage: 1,
+      patrimonies: [{
+        id: patrimony.id.toString(),
+        number: patrimony.number,
+        brand: patrimony.brand,
+        category: patrimony.category.name,
+        owner: patrimony.owner.name,
+        sector: patrimony.owner.sector.name
+      }]
+    }))
+  }
+
+  const setNotFound = (): void => {
+    setState(old => ({
+      ...old,
+      isLoading: false,
+      patrimonies: [],
+      mainError: 'Nada foi encontrado'
+    }))
+  }
+
+  const setPagination = (length: number): void => {
+    setState(old => ({
+      ...old,
+      isLoading: false,
+      totalPage: Math.ceil(length / state.take)
+    }))
+  }
+
   useEffect(() => {
     loadOwners.load()
       .then(owners => setState(old => ({
@@ -75,54 +123,33 @@ const PatrimonyList: React.FC<Props> = ({
 
   useEffect(() => {
     loadPatrimonies.load({})
-      .then((patrimonies) => {
-        setState(old => ({
-          ...old,
-          isLoading: false,
-          totalPage: Math.ceil(patrimonies.length / state.take)
-        }))
-      })
+      .then((patrimonies) => setPagination(patrimonies.length))
       .catch((error) => handleError(error))
   }, [state.reload])
 
   useEffect(() => {
     loadPatrimonies.load({ skip: state.skip, take: state.take })
-      .then((patrimonies) => {
-        setState(old => ({
-          ...old,
-          isLoading: false,
-          patrimonies: patrimonies.map((p) => ({
-            id: p.id.toString(),
-            brand: p.brand,
-            number: p.number,
-            category: p.category.name,
-            owner: p.owner.name,
-            sector: p.owner.sector.name
-          }))
-        }))
-      })
+      .then((patrimonies) => setPatrimonies(patrimonies))
       .catch((error) => handleError(error))
   }, [state.skip, state.take, state.reload])
 
   useEffect(() => {
+    if (state.category) {
+      loadPatrimoniesByCategoryId.loadByCategoryId({
+        id: Number(state.category),
+        skip: state.skip,
+        take: state.take
+      })
+        .then((patrimonies) => patrimonies ? setPatrimonies(patrimonies) : setNotFound())
+        .catch((error) => handleError(error))
+    }
+  }, [state.take, state.skip, state.category])
+
+  useEffect(() => {
     if (state.number) {
       loadPatrimonyByNumber.loadByNumber({ number: state.number })
-        .then((patrimony) => {
-          if (patrimony) {
-            setState(old => ({
-              ...old,
-              totalPage: 1,
-              patrimonies: [{
-                id: patrimony.id.toString(),
-                number: patrimony.number,
-                brand: patrimony.brand,
-                category: patrimony.category.name,
-                owner: patrimony.owner.name,
-                sector: patrimony.owner.sector.name
-              }]
-            }))
-          }
-        })
+        .then((patrimony) => { if (patrimony) setPatrimony(patrimony) })
+        .catch(error => handleError(error))
     }
   }, [state.number])
 
@@ -131,38 +158,6 @@ const PatrimonyList: React.FC<Props> = ({
       setState(old => ({ ...old, reload: !state.reload, mainError: '' }))
     }
   }, [state.category, state.number])
-
-  useEffect(() => {
-    if (state.category) {
-      loadPatrimoniesByCategoryId.loadByCategoryId({
-        id: Number(state.category),
-        skip: state.skip,
-        take: state.take
-      }).then((patrimonies) => {
-        if (patrimonies) {
-          setState(old => ({
-            ...old,
-            isLoading: false,
-            patrimonies: patrimonies.map((p) => ({
-              id: p.id.toString(),
-              brand: p.brand,
-              number: p.number,
-              category: p.category.name,
-              owner: p.owner.name,
-              sector: p.owner.sector.name
-            }))
-          }))
-        } else {
-          setState(old => ({
-            ...old,
-            isLoading: false,
-            patrimonies: [],
-            mainError: 'Nada foi encontrado'
-          }))
-        }
-      })
-    }
-  }, [state.take, state.skip, state.category])
 
   return (
     <div className="patrimony-list-wrap" data-testid="patrimonies">
