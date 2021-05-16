67a5ca4a4226a91797d18393214b58814605a721
vineboneto <vineboneto@gmail.com>
2021-05-16 02:07:13 -0300

    feat: create usecase LoadPatrimoniesByOwnerId

---
+++ src/domain/usecases/load-patrimonies-by-owner-id.ts
@@ -0,0 +1,14 @@
+import { PatrimonyModel } from '@/domain/models'
+
+export interface LoadPatrimoniesByOwnerId {
+  loadByOwnerId (params: LoadPatrimoniesByOwnerId.Params): Promise<LoadPatrimoniesByOwnerId.Model[]>
+}
+
+export namespace LoadPatrimoniesByOwnerId {
+  export type Params = {
+    id: number
+    skip?: number
+    take?: number
+  }
+  export type Model = PatrimonyModel
+}
