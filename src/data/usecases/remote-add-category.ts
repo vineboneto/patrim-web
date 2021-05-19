50a52decb5e5759e02612f3f6a6d13407abe3e11
vineboneto <vineboneto@gmail.com>
2021-05-19 03:06:11 -0300

    feat: implements add category

---
+++ src/data/usecases/remote-add-category.ts
@@ -0,0 +1,24 @@
+import { AddCategory } from '@/domain/usecases'
+import { HttpClient, HttpStatusCode } from '@/data/protocols'
+import { AccessDeniedError, UnexpectedError, UnprocessableEntityError } from '@/domain/errors'
+
+export class RemoteAddCategory implements AddCategory {
+  constructor (
+    private readonly httpClient: HttpClient,
+    private readonly url: string
+  ) {}
+
+  async add (params: AddCategory.Params): Promise<AddCategory.Model> {
+    const httpResponse = await this.httpClient.request({
+      body: params,
+      method: 'post',
+      url: this.url
+    })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return httpResponse.body
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      case HttpStatusCode.unprocessableEntity: throw new UnprocessableEntityError('nome', params.name)
+      default: throw new UnexpectedError()
+    }
+  }
+}
