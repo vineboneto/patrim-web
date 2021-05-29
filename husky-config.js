ded6849485b1b62d1ea2cab34731543762fc8fbc
vineboneto <vineboneto@gmail.com>
2021-05-29 15:39:10 -0300

    fix: bug of config husky-config

--- husky-config.js
+++ husky-config.js
@@ -2,19 +2,19 @@ const { execSync } = require('child_process')
 
 console.log('Setting Husky...')
 const scripts = [
-  'npx rimraf .config',
-  'npx husky install .config/husky',
-  'npx husky add .config/husky/pre-commit "yarn lint-staged --config .lintstagedrc.json"',
-  'npx husky add .config/husky/pre-push "yarn test:ci"',
-  'npx husky add .config/husky/commit-msg ".git/hooks/commit-msg $1"'
+  'npx rimraf .husky',
+  'npx husky install ',
+  'npx husky add .husky/pre-commit "yarn lint-staged --config .lintstagedrc.json"',
+  'npx husky add .husky/pre-push "yarn test:ci"',
+  'npx husky add .husky/commit-msg ".git/hooks/commit-msg $1"'
 ]
 
-for (script of scripts) {
+for (const script of scripts) {
   try {
     console.log(script + '👀')
     execSync(script)
     console.log('✔')
   } catch (error) {
-    console.error
+    console.error()
   }
 }
