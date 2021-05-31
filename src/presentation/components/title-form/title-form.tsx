8c0e7c88d00d33923d6b4a5a90bc53922dd1de17
vineboneto <vineboneto@gmail.com>
2021-05-31 03:43:12 -0300

    fix: add children to title form

--- src/presentation/components/title-form/title-form.tsx
+++ src/presentation/components/title-form/title-form.tsx
@@ -2,6 +2,6 @@ import './title-form-styles.css'
 
 import React from 'react'
 
-const TitleForm: React.FC = () => <h2 className="title-form">Nova Categoria</h2>
+const TitleForm: React.FC = ({ children }: any) => <h2 className="title-form">{children}</h2>
 
 export default TitleForm
